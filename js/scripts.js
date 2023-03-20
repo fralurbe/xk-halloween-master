919cc3eb306facbc6e172574212bd8d25cf06351
Fran Lurbe <francisco.lurbe@gmail.com>
2023-03-20 14:59:12 +0100

    fin

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 // initialized AOS Librery
 AOS.init();
 
+let menu_bar = document.getElementById('menu_bar');
+let menu = document.getElementById('menu');
+
+menu_bar.addEventListener('click', ()=> {
+    menu.classList.toggle('show-menu')
+});
+
